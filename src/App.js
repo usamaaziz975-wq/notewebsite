470f901ea2f58a337ad5dabf8d286fd80ca9c551
usamaaziz7-design <usamaaziz.edu@gmail.com>
2026-04-19 18:21:04 +0500

    updated css and ui

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import "./App.css";
 import "./style.css";
 
 export default function App() {
